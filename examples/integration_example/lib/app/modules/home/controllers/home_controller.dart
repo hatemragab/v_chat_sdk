471acafe62ecda180059989a5b231f3ba87c7880
hatemragap <hatemragap5@gmail.com>
2022-11-20 00:27:51 +0200

    add more packages and examples

---
+++ examples/integration_example/lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  //TODO: Implement HomeController
+
+  final count = 0.obs;
+
+
+
+  void increment() => count.value++;
+}
