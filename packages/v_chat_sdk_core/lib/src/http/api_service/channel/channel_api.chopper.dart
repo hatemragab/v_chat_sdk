a4e2d3b6ab397eea0843d4d823aab7c82cbee947
hatemragap <hatemragap5@gmail.com>
2022-12-10 19:42:44 +0200

    add channel service with message service

---
+++ packages/v_chat_sdk_core/lib/src/http/api_service/channel/channel_api.chopper.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'channel_api.dart';
+
+// **************************************************************************
+// ChopperGenerator
+// **************************************************************************
+
+// ignore_for_file: always_put_control_body_on_new_line, always_specify_types, prefer_const_declarations, unnecessary_brace_in_string_interps
+class _$ChannelApi extends ChannelApi {
+  _$ChannelApi([ChopperClient? client]) {
+    if (client == null) return;
+    this.client = client;
+  }
+
+  @override
+  final definitionType = ChannelApi;
+}
